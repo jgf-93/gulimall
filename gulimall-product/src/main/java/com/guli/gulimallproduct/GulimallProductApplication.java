bfa55c5025faca4b6761e8aab1809834a6f7b486
jgf <1186967953@qq.com>
2024-06-03 15:56:00 +0800

    解决报错和冲突

--- gulimall-product/src/main/java/com/guli/gulimallproduct/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/guli/gulimallproduct/GulimallProductApplication.java
@@ -1,8 +1,10 @@
 package com.guli.gulimallproduct;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.guli.gulimallproduct.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
